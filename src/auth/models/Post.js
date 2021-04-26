cc7241bbc281d0b528bbab8f9c0df85acce7970c
Omar Hakem <youtarawneh997@gmail.com>
2021-04-26 08:17:02 +0300

    new schemas for all database and interface to interacte with it

---
+++ src/auth/models/Post.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const { Schema, model } = require('mongoose');
+
+const postSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+});
+
+module.exports = model('Post', postSchema);
