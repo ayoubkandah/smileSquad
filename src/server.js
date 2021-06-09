7f39671e530fcbf8291f2723556ac004871819b7
ayoub kandah <76391749+ayoubkandah@users.noreply.github.com>
2021-06-09 23:04:15 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://smile-squad.netlify.app/",
     methods: ["GET", "POST"]
   }
 });
